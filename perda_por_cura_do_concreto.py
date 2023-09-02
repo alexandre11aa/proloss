c306cf0011b7e3f6342440cb69009c5b0e269586
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2023-09-02 11:31:09 -0300

    Create perda_por_cura_do_concreto.py

---
+++ perda_por_cura_do_concreto.py
@@ -0,0 +1,56 @@
+import math
+
+def cura_do_concreto(Ac, ycin, Ic, 
+                     Ep, Ap, n_fios, 
+                     fck28, Mg, n, 
+                     dias, forcas, altura):
+
+    # Módulos de elasticidade do concreto
+
+    fcks, Ecis = [[],[]]
+
+    for i in range(len(dias)):
+        fcks.append(fck28 * math.e**(0.25 * (1 - (28 / dias[i])**(1/2))))
+        Ecis.append(5600 * (fcks[i])**(1/2))
+
+    Eci_med = sum(Ecis) / len(Ecis)
+
+    alpha_p = Ep * 1000 / Eci_med
+
+    # Tensões produzidas por todos os cabos
+
+    y0, NP0i_epi = [0, 0]
+
+    alturas_unicas = list(set(altura))
+
+    for i in range(len(alturas_unicas)):
+        n_1, n_2 = [0, 0]
+
+        for j in range(len(altura)):
+            if alturas_unicas[i] == altura[j]:
+                n_1 += 1
+                n_2 += forcas[j]
+            
+        y0 += (alturas_unicas[i] / 100) * n
+
+        NP0i_epi += n_2 * (ycin - (alturas_unicas[i] / 100))
+
+    y0 /= len(altura)
+
+    ocp = (sum(forcas) / Ac) + ((NP0i_epi / Ic) * (ycin - y0))
+
+    # Tensão mobilizada pela protenção no C.G. de Ap.
+
+    ocg = Mg * (ycin - y0) / Ic
+
+    # Perda média de protensão por encurtamento imediato do concreto
+
+    delta_op = ((n - 1) / (n * 2)) * alpha_p * (ocp + ocg)
+
+    # Perda da força de protenção para cabos
+
+    perda_de_forca_todos_os_cabos = []
+    for i in range(len(forcas)):
+        perda_de_forca_todos_os_cabos.append(forcas[i] - n_fios * Ap * 10**(-4) * delta_op)
+
+    return ocp, ocg, delta_op, perda_de_forca_todos_os_cabos
