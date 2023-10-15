23f14f2f5663a0f1509aeda2b20a67916ead3960
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2023-10-15 16:52:36 -0300

    Update retracao_do_concreto.py

--- perdas_progressivas/retracao_do_concreto.py
+++ perdas_progressivas/retracao_do_concreto.py
@@ -4,30 +4,30 @@ Cálculo de perda de protensão por retração do concreto explicado por Cholfe 
 CHOLFE, L.; BONILHA, L. Concreto Protendido: teoria e prática. São Paulo: Pini, 2013. Páginas 164-169.
 '''
 
-def retracao_do_concreto(U, t0, t, h, abatimento, Bs_t0, Bs_t):
+def calculo_de_Bst(t_fic, h_fic):
 
-    def calculo_de_Bst(t_fic, h_fic):
+    if h_fic / 100 <= 0.05:
+        h_fic = 0.05
 
-        if h_fic / 100 <= 0.05:
-            h_fic = 0.05
+    elif h_fic / 100 >= 1.6:
+        h_fic = 1.6
 
-        elif h_fic / 100 >= 1.6:
-            h_fic = 1.6
+    else:
+        h_fic /= 100
 
-        else:
-            h_fic /= 100
+    A = 40
+    B = 116 * h_fic**3 - 282 * h_fic**2 + 220 * h_fic - 4.8
+    C = 2.5 * h_fic**3 - 8.8 * h_fic + 40.7
+    D = -75 * h_fic**3 + 585 * h_fic**2 + 496 * h_fic - 6.8
+    E = -169 * h_fic**4 + 88 * h_fic**3 + 584 * h_fic**2 - 39 * h_fic + 0.8
 
-        A = 40
-        B = 116 * h_fic**3 - 282 * h_fic**2 + 220 * h_fic - 4.8
-        C = 2.5 * h_fic**3 - 8.8 * h_fic + 40.7
-        D = -75 * h_fic**3 + 585 * h_fic**2 + 496 * h_fic - 6.8
-        E = -169 * h_fic**4 + 88 * h_fic**3 + 584 * h_fic**2 - 39 * h_fic + 0.8
+    t_1 = (t_fic / 100)**3
+    t_2 = (t_fic / 100)**2
+    t_3 = (t_fic / 100)
 
-        t_1 = (t_fic / 100)**3
-        t_2 = (t_fic / 100)**2
-        t_3 = (t_fic / 100)
+    return (t_1 + A * t_2 + B * t_3) / (t_1 + C * t_2 + D * t_3 + E)
 
-        return (t_1 + A * t_2 + B * t_3) / (t_1 + C * t_2 + D * t_3 + E)
+def retracao_do_concreto(U, t0, t, h, abatimento):
 
     ecs = []
 
@@ -52,22 +52,16 @@ def retracao_do_concreto(U, t0, t, h, abatimento, Bs_t0, Bs_t):
 
         # Terceira Variável
 
-        if t0 != [] and t != []:
+        Bs_t0 = calculo_de_Bst(t0[i], h[i])
 
-            Bs_t0 = calculo_de_Bst(t0[i], h[i])
+        if t[i] == '∞':
+            Bs_t = 1
+        
+        else:
+            Bs_t = calculo_de_Bst(t[i], h[i])
 
-            if t[i] == '∞':
-                Bs_t = 1
-            
-            else:
-                Bs_t = calculo_de_Bst(t[i], h[i])
+        # Resultado
 
-            # Resultado
-
-            ecs.append(e1s * e2s * (Bs_t - Bs_t0))
-
-        elif t0 == [] and t == []:
-
-            ecs.append(e1s * e2s * (Bs_t[i] - Bs_t0[i]))
+        ecs.append(e1s * e2s * (Bs_t - Bs_t0))
 
     return ecs
