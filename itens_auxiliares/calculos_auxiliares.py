c7803f212f05a993a04a7fa30764b7f60dcc2784
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2023-10-31 00:29:13 -0300

    Update calculos_auxiliares.py

--- itens_auxiliares/calculos_auxiliares.py
+++ itens_auxiliares/calculos_auxiliares.py
@@ -1,6 +1,6 @@
 import math
 
-# Espessura Fictícia
+# Espessura Fictícia - CHOLFE, L.; BONILHA, L. Concreto Protendido: teoria e prática. São Paulo: Pini, 2013. Páginas 160, 162.
 
 def calculo_da_espessura_ficticia(Ac, u_ar, U):
 
@@ -12,13 +12,7 @@ def calculo_da_espessura_ficticia(Ac, u_ar, U):
 
     return h_fic
 
-'''
-CHOLFE, L.; BONILHA, L. Concreto Protendido: teoria e prática. São Paulo: Pini, 2013. Páginas 160, 162.
-
-print('Aplicação numérica (Espessura fictícia): ' + str(calculo_da_espessura_ficticia(0.86, 8.2, [40, 60, 80])))
-'''
-
-# Idade Fictícia
+# Idade Fictícia - CHOLFE, L.; BONILHA, L. Concreto Protendido: teoria e prática. São Paulo: Pini, 2013. Páginas 161, 163.
 
 def calculo_da_idade_ficticia(a, Ti, delta_t):
     
@@ -36,13 +30,7 @@ def calculo_da_idade_ficticia(a, Ti, delta_t):
 
     return t_fic
 
-'''
-CHOLFE, L.; BONILHA, L. Concreto Protendido: teoria e prática. São Paulo: Pini, 2013. Páginas 161, 163.
-
-print('Aplicação numérica (Idade fictícia): ' + str(calculo_da_idade_ficticia([2, 1, 3], [30, 26, 20], [7, 12, 9])))
-'''
-
-# Efeito Conjunto de Retração e Fluência
+# Efeito Conjunto de Retração e Fluência - CHOLFE, L.; BONILHA, L. Concreto Protendido: teoria e prática. São Paulo: Pini, 2013. Páginas 182-189.
 
 def efeito_conjunto_retracao_e_fluencia(Ep, Ecs, Ecc, Eci28, ocP0, oP0, y):
 
