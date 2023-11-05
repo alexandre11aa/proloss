40f2770e58eea4606a2ce53a0d94442fb32d629d
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2023-11-05 14:31:27 -0300

    Update calculos_auxiliares.py

--- itens_auxiliares/calculos_auxiliares.py
+++ itens_auxiliares/calculos_auxiliares.py
@@ -4,9 +4,14 @@ import math
 
 def calculo_da_espessura_ficticia(Ac, u_ar, U):
 
+    Ac = float(Ac)
+    u_ar = float(u_ar)
+
     h_fic = []
 
     for i in range(len(U)):
+
+        U[i] = float(U[i])
 
         h_fic.append(100 * (1 + math.e**(-7.8 + 0.1 * U[i])) * (2 * Ac) / u_ar)
 
@@ -20,11 +25,16 @@ def calculo_da_idade_ficticia(a, Ti, delta_t):
 
     for i in range(len(Ti)):
 
+        Ti[i] = float(Ti[i])
+        delta_t[i] = float(delta_t[i])
+
         constante += ((Ti[i] + 10) / 30) * delta_t[i]
 
     t_fic = []
 
     for i in range(len(a)):
+
+        a[i] = float(a[i])
 
         t_fic.append('%.4f' % (a[i] * constante))
 
@@ -44,6 +54,14 @@ def efeito_conjunto_retracao_e_fluencia(Ep, Ecs, Ecc, Eci28, ocP0, oP0, y):
 
     for i in range(len(Ep)):
 
+        Ep[i] = float(Ep[i])
+        Ecs[i] = float(Ecs[i])
+        Ecc[i] = float(Ecc[i])
+        Eci28[i] = float(Eci28[i])
+        ocP0[i] = float(ocP0[i])
+        oP0[i] = float(oP0[i])
+        y[i] = float(y[i])
+
         lista_de_resultados.append("%.2f" % ((Ep[i] * ajuste_1 * (Ecs[i] * ajuste_2 + Ecc[i] * ajuste_2)) / (1 - (Ep[i] * ajuste_1 * ocP0[i]  / (Eci28[i] * ajuste_1 * oP0[i])) * (1 + y[i] / 2))))
 
     return lista_de_resultados
@@ -51,5 +69,8 @@ def efeito_conjunto_retracao_e_fluencia(Ep, Ecs, Ecc, Eci28, ocP0, oP0, y):
 # Método Geral para Perda de Tensão - CHOLFE, L.; BONILHA, L. Concreto Protendido: teoria e prática. São Paulo: Pini, 2013. Páginas 196-202.
 
 def perda_de_tensao_pelo_metodo_geral_funcoes(delta_cs, delta_rrel):
+
+    delta_cs = float(delta_cs)
+    delta_rrel = float(delta_rrel)
 
     return '%.2f' % (delta_cs + delta_rrel)
